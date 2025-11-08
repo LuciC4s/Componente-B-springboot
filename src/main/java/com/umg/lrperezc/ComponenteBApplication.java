9d61267d233b6e1d05a4baf49d50c88f348dd0d9
Lucia Perez <lperezc24@miumg.edu.gt>
2025-11-08 00:02:40 -0600

    first commit

---
+++ src/main/java/com/umg/lrperezc/ComponenteBApplication.java
@@ -0,0 +1,13 @@
+package com.umg.lrperezc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponenteBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComponenteBApplication.class, args);
+    }
+
+}
